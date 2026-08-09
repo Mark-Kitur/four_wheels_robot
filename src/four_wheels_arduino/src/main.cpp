83e3188e4e7ea6765eaca099db0030bb70a168fb
KolKemboi <kolkemboi@gmail.com>
2026-08-09 12:52:59 +0300

    arduino code

---
+++ src/four_wheels_arduino/src/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+// 
+float left_position = 0.0;
+float left_velocity = 0.0;
+float right_position = 0.0;
+float right_velocity = 0.0;
+
+void setup() {
+  Serial.begin(115200);
+// Example values (replace with your encoder readings)
+  
+  // Wait for the serial port (useful on boards with native USB)
+  while (!Serial) {
+  }
+}
+
+void loop() {
+  
+if (Serial.available())
+{
+    String line = Serial.readStringUntil('\n');
+
+    float left;
+    float right;
+
+    if (sscanf(line.c_str(), "%f,%f",
+               &left, &right) == 2)
+    {
+        // Drive motors
+    }
+}
+  Serial.print(left_position);
+  Serial.print(",");
+
+  Serial.print(left_velocity);
+  Serial.print(",");
+
+  Serial.print(right_position);
+  Serial.print(",");
+
+  Serial.println(right_velocity);
+
+  delay(20);   // 50 Hz
+}
