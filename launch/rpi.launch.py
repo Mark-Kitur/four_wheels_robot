79b310ae1a97193003ab88279fb24b896abd0ef9
Mark-Kitur <kimutai.workspace@gmail.com>
2026-08-09 11:27:26 +0300

    fixed

--- launch/rpi.launch.py
+++ launch/rpi.launch.py
@@ -11,6 +11,8 @@ def generate_launch_description():
     xacro_file = "model.urdf.xacro"
     xacro_file_path = os.path.join(pkg_name,"urdf", xacro_file)
 
+    controllers = os.path.join(pkg_name,"config","wheels_controllers.yaml")
+
     robot_description = xacro.process_file(xacro_file_path).toxml()
 
     robot_state_publisher = Node(
@@ -25,43 +27,49 @@ def generate_launch_description():
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        output="screen",
+        output="both",
         parameters=[
             {"robot_description": robot_description},
             controllers
         ]
     )
+    
 
     # Controllers Joint broadcaster, IMU, Diff drive
-    joint_state_broadcaster_spawner =Node(
-        name="joint_state_broadcaster",
-        executable="spawner",
-        arguments=['joint_state_broadcaster']
-    )
+    joint_state_broadcaster_spawner = Node(
+    package="controller_manager",
+    executable="spawner",
+    name="joint_state_broadcaster_spawner",
+    arguments=[
+        "joint_state_broadcaster",
+        "--controller-manager",
+        "/controller_manager"
+    ]
+)
 
 
-    imu_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=['imu_sensor_broadcaster']
-    )
+    # imu_broadcaster_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=['imu_sensor_broadcaster']
+    # )
 
     velocity_spawner=Node(
         package="controller_manager",
         executable="spawner",
-        arguments=['velocity_controller']
+        arguments=['velocity_controller',"--controller-manager",'/controller_manager']
     )
 
     ld = LaunchDescription()
 
-    timer_action= TimerAction(
-        period=3.0,
+    timer_action = TimerAction(
+    period=3.0,
         actions=[
-            joint_state_broadcaster_spawner, imu_broadcaster_spawner, velocity_spawner
+            joint_state_broadcaster_spawner,
+            velocity_spawner,
         ]
     )
 
-    
     ld.add_action(robot_state_publisher)
     ld.add_action(controller_manager)
     ld.add_action(timer_action)
